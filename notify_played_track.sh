0b429b2d80c1e3366909fc36bab5df89b346aa29
jpietrzyk <jacpie3k@gmail.com>
2018-03-11 02:29:24 +0100

    Update playback/player notifications scripts

--- notify_played_track.sh
+++ notify_played_track.sh
@@ -4,7 +4,11 @@ COVERS_PATH=~/tmp/covers/
 ARTIST=$(playerctl metadata xesam:artist)
 ALBUM=$(playerctl metadata xesam:album)
 TRACK=$(playerctl metadata xesam:title)
+RATING=$(playerctl metadata xesam:autoRating)
+MESSAGE="<b>Artist:</b> $ARTIST \n$ALBUM $RATING"
+STATUS=$(playerctl status)
 
 wget -nc -q -P $COVERS_PATH $COVER_URL
 
-notify-send -i "$COVERS_PATH$COVER_FILE_NAME" -t 100 "$TRACK" "$ARTIST from $ALBUM"
+notify-send -i "$COVERS_PATH$COVER_FILE_NAME" -t 100 -u normal -h string:player:info "$STATUS: $TRACK" "$MESSAGE"
+
--- notify_volume.sh
+++ notify_volume.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VOLUME_LOCAL_NAME='Poziom głośności'
 MESSAGE='Updated to: '
+
 volume=$(echo `(pactl list sinks | grep "$VOLUME_LOCAL_NAME:")| awk '{print $6}'`)
-notify-send -i /usr/share/icons/gnome/32x32/categories/gnome-multimedia.png -t 100 "Volume" "$MESSAGE: $volume"
+notify-send -i /usr/share/icons/gnome/256x256/devices/audio-speakers.png -t 100 -u normal -h string:volume:info "Volume" "$MESSAGE: $volume"
