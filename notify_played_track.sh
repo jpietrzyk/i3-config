6a757c7ba3a6a5af7636cfe83effea0c4deec597
jpietrzyk <jacpie3k@gmail.com>
2018-03-11 22:48:56 +0100

    Replace notify-osd with dunst

--- notify_played_track.sh
+++ notify_played_track.sh
@@ -1,14 +1,16 @@
 COVER_URL=$(playerctl metadata mpris:artUrl)
-COVER_FILE_NAME=${COVER_URL##*/}
+COVER_FILE_NAME="${COVER_URL##*/}.png"
 COVERS_PATH=~/tmp/covers/
 ARTIST=$(playerctl metadata xesam:artist)
 ALBUM=$(playerctl metadata xesam:album)
 TRACK=$(playerctl metadata xesam:title)
 RATING=$(playerctl metadata xesam:autoRating)
-MESSAGE="<b>Artist:</b> $ARTIST \n$ALBUM $RATING"
+MESSAGE="$ARTIST ($ALBUM)"
 STATUS=$(playerctl status)
 
-wget -nc -q -P $COVERS_PATH $COVER_URL
+echo $COVER_FILE_NAME
 
-notify-send -i "$COVERS_PATH$COVER_FILE_NAME" -t 100 -u normal -h string:player:info "$STATUS: $TRACK" "$MESSAGE"
+wget -nc -q -O "$COVERS_PATH/$COVER_FILE_NAME" $COVER_URL
+
+notify-send -i "$COVERS_PATH$COVER_FILE_NAME" -t 1000 -u normal -h string:player:info "$STATUS: $TRACK" "$MESSAGE"
 
--- notify_volume.sh
+++ notify_volume.sh
@@ -3,4 +3,4 @@ VOLUME_LOCAL_NAME='Poziom głośności'
 MESSAGE='Updated to: '
 
 volume=$(echo `(pactl list sinks | grep "$VOLUME_LOCAL_NAME:")| awk '{print $6}'`)
-notify-send -i /usr/share/icons/gnome/256x256/devices/audio-speakers.png -t 100 -u normal -h string:volume:info "Volume" "$MESSAGE: $volume"
+notify-send -i /usr/share/icons/gnome/48x48/devices/audio-speakers.png -t 1000 -u normal -h string:volume:info "Volume" "$MESSAGE: $volume"
